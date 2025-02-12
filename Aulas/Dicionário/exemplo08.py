c8d706e22365e5ce6c006ccf43715e527a63a0fa
Liedson G. V. de Aquino <l.g.v.a.1700@gmail.com>
2025-02-12 20:57:09 -0300

    teste do exemplo 08

---
+++ Aulas/Dicionário/exemplo08.py
@@ -0,0 +1,51 @@
+
+'''
+   EXEMPLO 08:
+
+   Elabore um programa em Python que permita o cadastro de alunos e suas respectivas 
+   disciplinas cursadas, bem como as notas obtidas em cada uma delas. 
+   O programa deve funcionar da seguinte forma:
+
+   - O usuário deverá informar a matrícula de um aluno e as disciplinas que ele já cursou;
+   
+   - Para cada disciplina, o programa solicitará duas notas;
+   
+   - O programa continuará pedindo disciplinas para o aluno até que o nome da disciplina 
+     seja "FIM". Quando isso ocorrer, o processo de cadastro desse aluno será finalizado;
+   
+   - Caso a matrícula informada já tenha sido cadastrada anteriormente, o programa deverá 
+     exibir a mensagem "MATRÍCULA JÁ CADASTRADA" e não permitirá o cadastro de novas disciplinas 
+     para essa matrícula;
+   
+   - Caso uma disciplina já tenha sido cadastrada para a matrícula de um aluno, o programa deverá 
+     exibir uma mensagem avisando que a disciplina já foi registrada e pedirá para o usuário 
+     informar outra;
+   
+   - Utilize como padrão de notas valores inteiros entre 0 e 100;
+
+   - O programa deverá continuar solicitando novas matrículas de alunos até que o usuário 
+     opte por parar;
+   
+   - Ao final, o programa deverá exibir o dicionário com as matrículas dos alunos, as disciplinas que 
+     cursaram e as respectivas notas.
+'''
+
+import sys
+
+dictAlunos = dict()
+
+matricula = input('Digite a matrícula: ')
+'''if matricula == dictAlunos[0]:
+  sys.exit('Matrícula já cadastrada')
+'''
+while True:
+  disciplina = input('Digite a disciplina: ').upper()
+  if disciplina == 'FIM': break
+
+  if disciplina in matricula:
+    print('Disciplina já cadastrada. \nInforme outra disciplina.')
+  nota = input('Digite a nota da disciplina: ')
+  dictAluno = {matricula[0]: {'Disciplina': nota[1]}
+                for matricula in dictAlunos  }
+
+print(dictAluno)
